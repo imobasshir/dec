ed7441bcce1f19ef7ed777d3cbdbe685229b6d17
Arun <arunsingh641997@gmail.com>
2021-11-13 13:44:22 +0530

    notes removed

--- javascript/index.js
+++ javascript/index.js
@@ -23,6 +23,7 @@ function visible1() {
 var modal = document.getElementById("myModal");
 var modal1 = document.getElementById("myModal1");
 var modal2 = document.getElementById("myModal2");
+var modal3 = document.getElementById("myModal3");
 
 var btn = document.getElementById("myBtn");
 var btn1 = document.getElementById("myBtn1");
@@ -31,6 +32,7 @@ var btn2 = document.getElementById("myBtn2");
 var span = document.getElementsByClassName("close")[0];
 var span1 = document.getElementsByClassName("close1")[0];
 var span2 = document.getElementsByClassName("close2")[0];
+var span3 = document.getElementsByClassName("close3")[0];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function () {
@@ -47,16 +49,19 @@ btn2.onclick = function () {
 span.onclick = function () {
     modal.style.display = "none";
 }
-span.onclick = function () {
+span2.onclick = function () {
     modal2.style.display = "none";
 }
 span1.onclick = function () {
     modal1.style.display = "none";
 }
+span3.onclick = function () {
+    modal3.style.display = "none";
+}
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-    if (event.target == modal) {
+    if (event.target == modal ) {
         modal.style.display = "none";
     }
 }
@@ -70,10 +75,16 @@ window.onclick = function (event) {
         modal1.style.display = "none";
     }
 }
+window.onclick = function (event) {
+    if (event.target == modal3) {
+        modal3.style.display = "none";
+    }
+}
 
 function signup(){
     modal1.style.display="block";
     modal.style.display="none";
+    modal3.style.display="none";
     window.onclick = function (event) {
         if (event.target == modal1) {
             modal1.style.display = "none";
@@ -85,12 +96,16 @@ function signup(){
 function signin(){
     modal.style.display="block";
     modal1.style.display="none";
+    modal3.style.display="none";
     window.onclick = function (event) {
         if (event.target == modal) {
             modal.style.display = "none";
         }
     }
     
+}
+function error(){
+    modal3.style.display="block";
 }
 
   //sign in ends here 
